775aabe5673bd6747c559cccfcc7776ea603a4ab
trevorimrokfan-a11y <trevor.imrokfan@gmail.com>
2026-05-19 12:18:05 +0800

    rebuild: update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-
-import { HashRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import "./index.css";
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import HomePage from './pages/HomePage'
+import ProductsPage from './pages/ProductsPage'
+import ApplicationsPage from './pages/ApplicationsPage'
 
 export default function App() {
   return (
     <HashRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <div className="min-h-screen bg-[#0A0E1A] text-white flex flex-col">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/applications" element={<ApplicationsPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </HashRouter>
-  );
+  )
 }
