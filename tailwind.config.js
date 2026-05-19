989bae9e5072ae2fc1fbd7dadf0069b9fea53f5b
trevorimrokfan-a11y <trevor.imrokfan@gmail.com>
2026-05-19 12:17:58 +0800

    rebuild: update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
-
 export default {
-  darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        brand: '#2563EB',
+        'brand-light': '#3B82F6',
+        dark: '#0A0E1A',
+        'dark-2': '#0F1624',
+        'dark-3': '#151D2E',
+        'dark-card': '#111827',
+      },
+    },
   },
   plugins: [],
 }
